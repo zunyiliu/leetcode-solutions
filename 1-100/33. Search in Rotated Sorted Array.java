c5f3f568b84cb69e97660fbaee1c027406519a84
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-11-29 23:20:37 +1030

    Update and rename 33. Search in Rotated Sorted Array to 33. Search in Rotated Sorted Array.java

--- 1-100/33. Search in Rotated Sorted Array.java
+++ 1-100/33. Search in Rotated Sorted Array.java
@@ -1,4 +1,5 @@
 1. use mind of binary search -- though it runs fast, my solution is wordy, can be simplified in some logic 
+2. same way but more simplified
 
 class Solution {
     public int search(int[] nums, int target) {
@@ -43,6 +44,33 @@ class Solution {
                 r = mid-1;
             }
         }
+        return -1;
+    }
+}
+
+//solution 2
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        
+        while(l<r){
+            int mid = (l+r)/2;
+            if(nums[mid]>=nums[l]){
+                if(nums[l]<=target && target<=nums[mid]){
+                    r = mid;
+                }else{
+                    l = mid+1;
+                }
+            }else{
+                if(nums[mid]<=target && target<=nums[r]){
+                    l = mid;
+                }else{
+                    r = mid-1;
+                }
+            }
+        }
+        if(l==r && target == nums[l]) return l;
         return -1;
     }
 }
