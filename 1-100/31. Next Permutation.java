0b6fa660847a9aa9647a0d70d071005be281a184
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-11-30 01:10:00 +1030

    Update 31. Next Permutation.java

--- 1-100/31. Next Permutation.java
+++ 1-100/31. Next Permutation.java
@@ -1,11 +1,11 @@
-1. solution: your goal is to find the next larger permutation number of the current number(just a bit larger than it, means there's no
-other permutations can exist between these two numbers). The mind behind the solution is: you start from the end of the number, assume it
-is descending sorted, if you go through all the list and find it's totally descended, then reverse the whole list(to get the smallest one).
-Else, the number which firstly breaches descending sequence will be selected, and it will exchange with the number in the list of scanned
-numbers that is just a one step bigger than itself, after exchanging the scanned list of course will still be descending sorted, then you
-reverse the descending sorted scanned list.
-E.G. 548632 --> first breached descending sequence's number is 4 --> go through scanned list 8632, find 6 wich is just bit larger than 4
---> exchange 4 with 6 to get 568432, reverse 8432 to get 562348 --> done!!
+// 1. solution: your goal is to find the next larger permutation number of the current number(just a bit larger than it, means there's no
+// other permutations can exist between these two numbers). The mind behind the solution is: you start from the end of the number, assume it
+// is descending sorted, if you go through all the list and find it's totally descended, then reverse the whole list(to get the smallest one).
+// Else, the number which firstly breaches descending sequence will be selected, and it will exchange with the number in the list of scanned
+// numbers that is just a one step bigger than itself, after exchanging the scanned list of course will still be descending sorted, then you
+// reverse the descending sorted scanned list.
+// E.G. 548632 --> first breached descending sequence's number is 4 --> go through scanned list 8632, find 6 wich is just bit larger than 4
+// --> exchange 4 with 6 to get 568432, reverse 8432 to get 562348 --> done!!
 
 class Solution {
     public void nextPermutation(int[] nums) {
