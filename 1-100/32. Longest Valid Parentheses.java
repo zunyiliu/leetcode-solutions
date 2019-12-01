09b6b605179c8e44f7fbf023436bbba4ce51e5fd
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-12-01 15:30:37 +1030

    Create 32. Longest Valid Parentheses.java

---
+++ 1-100/32. Longest Valid Parentheses.java
@@ -0,0 +1,27 @@
+//dynamic programming&stack: use stack to store the index of '(' in s, 
+//an array where array[i] stores the longest valid parentheses end with index i, all end with '(' is by default 0
+//each time meet a ')' with index l try to pop a '(' with index r in stack, if stack is empty then no valid parentheses end with r
+//thus array[r] = 0, if stack is not empty then r-l+previous longest parenthese will be stored in array[r]
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        int max = 0;
+        int array[] = new int[s.length()];
+        Stack<Integer> stack = new Stack<>();
+    
+        for(int i=0;i<s.length();i++){
+           if(s.charAt(i)=='('){
+               stack.push(i);
+           }else{
+               if(stack.isEmpty()){
+                   array[i] = 0;
+               }else{
+                    int l_bracket = stack.pop();
+                    array[i] = l_bracket==0? i-l_bracket+1 : i-l_bracket+1+array[l_bracket-1];
+                    max = array[i]>max? array[i]:max;
+               }
+           }
+        }
+        return max;
+    }
+}
