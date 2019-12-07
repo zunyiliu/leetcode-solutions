a96239b35ef3f7c04854df4943d9837716ecceb5
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-12-08 01:50:21 +1030

    Update 2. Add Two Numbers.java

--- 1-100/2. Add Two Numbers.java
+++ 1-100/2. Add Two Numbers.java
@@ -1,4 +1,5 @@
-// Linked List manipulation
+// Linked List manipulation, simple math
+// solution 2:just a bit simplify of code
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -39,6 +40,35 @@ class Solution {
                 cur = cur.next;
                 l1 = l1.next;
             }
+        }
+        if(carry>0){
+            cur.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
+
+//solution 2
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = 0;
+        int carry = 0;
+        ListNode cur = null;
+        ListNode head = null;
+        while(l1!=null || l2!=null){
+            int num1 = l1==null? 0:l1.val;
+            int num2 = l2==null? 0:l2.val;
+            sum = (carry+num1+num2)%10;
+            carry = (carry+num1+num2)/10;
+            if(cur==null){
+                head = new ListNode(sum);
+                cur = head;
+            }else{
+                cur.next = new ListNode(sum);
+                cur = cur.next;
+            }
+            if(l1!=null) l1=l1.next;
+            if(l2!=null) l2=l2.next;           
         }
         if(carry>0){
             cur.next = new ListNode(carry);
