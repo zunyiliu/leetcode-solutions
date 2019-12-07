c4f52527437a1e13a86da6c0b8e65a14f8bad524
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-12-08 01:18:43 +1030

    Update 1. Two Sum.java

--- 1-100/1. Two Sum.java
+++ 1-100/1. Two Sum.java
@@ -1,1 +1,36 @@
+//1. two pass hashmap
+//2. one pass hashmap, while putting <K,V> onto map, check if the pair has found(since there must exactly one pair)
 
+//solution 1
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> numbers = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            numbers.put(nums[i],i);
+        }
+        int res[] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            if( numbers.get(target-nums[i])!=null ){
+                res[0] = i;
+                res[1] = numbers.get(target-nums[i]);
+                if(res[0]!=res[1])
+                break;
+            }
+        }
+        return res;
+    }
+}
+
+//solution 2
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
