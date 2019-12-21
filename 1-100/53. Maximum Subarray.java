9209bf4030b41b5cbf6fda453e6f885509433ac0
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-12-21 12:29:11 +1030

    Update 53. Maximum Subarray.java

--- 1-100/53. Maximum Subarray.java
+++ 1-100/53. Maximum Subarray.java
@@ -1,3 +1,9 @@
+//O(n) solution 1: use a pointer to move forward the array, each time record the maximum value of the subarray, sum stands for
+// the previous array that contribute positive result(add up the max value), if sum<0, that means the previous array should no longer
+// be used, thus the subarray should be re-calculated from the next pointer
+
+
+// solution 1:
 class Solution {
     public int maxSubArray(int[] nums) {
         if(nums.length==0) return 0;
