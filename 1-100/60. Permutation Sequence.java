081d55fb516d24b422957dce060eaa96e27149dc
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-12-27 13:17:37 +1030

    Update 60. Permutation Sequence.java

--- 1-100/60. Permutation Sequence.java
+++ 1-100/60. Permutation Sequence.java
@@ -1,4 +1,7 @@
 // solution 1: backtrack, cant pass test since big n will cause stack overflow/time limit exceeds
+// solution 2: same method, a slight optimization so that the time complexity not exceeds the limit
+
+//solution 1
 class Solution {
     int count = 0;
     String res = "";
@@ -22,6 +25,32 @@ class Solution {
             String aTemp = a+b.charAt(i);
             String bTemp = b.substring(0,i)+b.substring(i+1,b.length());
             backtrack(aTemp,bTemp,k);
+        }
+    }
+}
+
+//solution 2
+class Solution {
+    int count = 0;
+    int K;
+    String res;
+    public String getPermutation(int n, int k) {
+        K = k;
+        backtrack("",new boolean[n+1]);
+        return res;
+    }
+    public void backtrack(String str,boolean mark[]){
+        if(str.length()==mark.length-1){
+            count++;
+            if(count==K) res = str;
+            return;
+        }
+        for(int i=1;i<mark.length;i++){
+            if(!mark[i]){
+                mark[i] = true;
+                backtrack(str+i,mark);
+                mark[i] = false;
+            }
         }
     }
 }
