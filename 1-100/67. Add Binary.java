145e93fa14938f14e8a6fdd3d63d36b2dc284b72
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2019-12-30 20:46:24 +1030

    Update 67. Add Binary.java

--- 1-100/67. Add Binary.java
+++ 1-100/67. Add Binary.java
@@ -1,5 +1,6 @@
 // easy 
 // solution 2: use StringBuilder instead of str+str operation
+// however from the running time seems StringBuilder is not faster than str plus str(they are same)
 
 //solution 1
 class Solution {
@@ -24,3 +25,23 @@ class Solution {
 }
 
 //solution 2
+class Solution {
+    public String addBinary(String a, String b) {
+        if(a.length()>b.length()){
+            String c = b;
+            b = a;
+            a = c;
+        }
+        int sum = 0,carry = 0;
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<b.length();i++){
+            int charA = i>=a.length()? 0:a.charAt(a.length()-i-1)-'0';
+            int charB = b.charAt(b.length()-i-1)-'0';
+            sum = (carry+charA+charB)%2;
+            carry = (carry+charA+charB)/2;
+            res.insert(0,sum);
+        }
+        if(carry==1) res.insert(0,1);
+        return res.toString();
+    }
+}
