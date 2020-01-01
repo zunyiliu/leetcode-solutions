adc0c8ff04a0c6ba1eedf39b77ed2d1f56476d32
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-01 19:31:09 +1030

    Update 70. Climbing Stairs.java

--- 1-100/70. Climbing Stairs.java
+++ 1-100/70. Climbing Stairs.java
@@ -16,3 +16,19 @@ class Solution {
         }
     }
 }
+
+//solution 2
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1||n==2) return n;
+        int num1 = 1;
+        int num2 = 2;
+        int cur = 0;
+        for(int i=3;i<=n;i++){
+            cur = num1+num2;
+            num1 = num2;
+            num2 = cur;
+        }
+        return cur;
+    }
+}
