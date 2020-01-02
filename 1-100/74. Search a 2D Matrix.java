04a27d96f5ed64593c31c02030245267dc41f8fb
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-02 23:02:35 +1030

    Update 74. Search a 2D Matrix.java

--- 1-100/74. Search a 2D Matrix.java
+++ 1-100/74. Search a 2D Matrix.java
@@ -30,3 +30,20 @@ class Solution {
 }
 
 // solution 2
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0) return false;
+        int start = 0;
+        int end = matrix.length * matrix[0].length-1;
+        
+        while(start<=end){
+            int mid = (start+end)/2;
+            int i = mid/matrix[0].length;
+            int j = mid%matrix[0].length;
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]<target) start = mid+1;
+            else end = mid-1;
+        }
+        return false;
+    }
+}
