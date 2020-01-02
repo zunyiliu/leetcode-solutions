a238f71b5f099fe80a0c7be00ad44c7a418ccfaa
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-02 23:32:41 +1030

    Update 75. Sort Colors.java

--- 1-100/75. Sort Colors.java
+++ 1-100/75. Sort Colors.java
@@ -21,3 +21,26 @@ class Solution {
         nums[i] = temp;
     }
 }
+
+//solution 2: more clear code
+class Solution {
+    public void sortColors(int[] nums) {
+        int p0 = 0;
+        int p2 = nums.length-1;
+        for(int i=0;i<=p2;i++){
+            if(nums[i]==0){
+                swap(nums,i,p0);
+                p0++;
+            }else if(nums[i]==2){
+                swap(nums,i,p2);
+                p2--;
+                i--;
+            }
+        }
+    }
+    public void swap(int[] nums,int p0,int i){
+        int temp = nums[p0];
+        nums[p0] = nums[i];
+        nums[i] = temp;
+    }
+}
