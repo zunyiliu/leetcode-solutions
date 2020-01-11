3f5c7d5fb780f38053f9e8f2fe1ea3c10c81d137
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-12 05:01:57 +1030

    Update 90. Subsets II.java

--- 1-100/90. Subsets II.java
+++ 1-100/90. Subsets II.java
@@ -1,3 +1,6 @@
+// solution 1 & 1.1: backtracking same concept but add time different
+
+//olution 1
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList();
@@ -12,6 +15,28 @@ class Solution {
                 if(i==st || nums[i-1]!=nums[i]){
                     cur.add(nums[i]);
                     res.add(new ArrayList(cur));
+                    recur(res,cur,i+1,nums);
+                    cur.remove(cur.size()-1);
+                }
+            }
+        }
+    }
+}
+
+//solution 1.1
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(nums);
+        recur(res,new ArrayList(),0,nums);
+        return res;
+    }
+    public void recur(List<List<Integer>> res,List<Integer> cur,int st,int[] nums){
+        res.add(new ArrayList(cur));
+        if(st<nums.length){
+            for(int i=st;i<nums.length;i++){
+                if(i==st || nums[i-1]!=nums[i]){
+                    cur.add(nums[i]);
                     recur(res,cur,i+1,nums);
                     cur.remove(cur.size()-1);
                 }
