681a70b0740e7b26acced0347f23e2ba2c70d856
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-12 04:59:33 +1030

    Create 90. Subsets II.java

---
+++ 1-100/90. Subsets II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(nums);
+        recur(res,new ArrayList(),0,nums);
+        res.add(new ArrayList());
+        return res;
+    }
+    public void recur(List<List<Integer>> res,List<Integer> cur,int st,int[] nums){
+        if(st<nums.length){
+            for(int i=st;i<nums.length;i++){
+                if(i==st || nums[i-1]!=nums[i]){
+                    cur.add(nums[i]);
+                    res.add(new ArrayList(cur));
+                    recur(res,cur,i+1,nums);
+                    cur.remove(cur.size()-1);
+                }
+            }
+        }
+    }
+}
