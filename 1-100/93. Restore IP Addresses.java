91f008a33da489461fc60b0925b7dad11eeae1ce
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-19 16:53:35 +1030

    Update 93. Restore IP Addresses.java

--- 1-100/93. Restore IP Addresses.java
+++ 1-100/93. Restore IP Addresses.java
@@ -1,5 +1,6 @@
 // solution 1: handwriting backtrack, for each bit try if it can generate a valid ip address, move forward if true, 
 // back track otherwise
+// solution 2: modification of solution 1 making it clearer
 class Solution {
 	public List<String> restoreIpAddresses(String s){
         List<String> res = new ArrayList();
@@ -38,5 +39,29 @@ class Solution {
     }
     public String dealstr(String s, int a, int b, int c){
         return s.substring(0,a)+"."+s.substring(a,b)+"."+s.substring(b,c)+"."+s.substring(c,s.length());
+    }
+}
+
+// solution 2
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList();
+        recur(res,s,0,"");
+        return res;
+    }
+    public void recur(List<String> res, String s, int count, String path){
+        if(count==4 || s.length()==0){
+            if(count==4 && s.length()==0) res.add(path.substring(0,path.length()-1));
+        }else{
+            int size = s.charAt(0)=='0'? 1:Math.min(s.length(),3);
+            for(int i=0;i<size;i++){
+                if(valid(s.substring(0,i+1)))
+                    recur(res,s.substring(i+1),count+1,path+s.substring(0,i+1)+'.');
+            }
+        }
+    }
+    public boolean valid(String s){
+        if(Integer.valueOf(s)>=0 && Integer.valueOf(s)<=255) return true;
+        return false;
     }
 }
