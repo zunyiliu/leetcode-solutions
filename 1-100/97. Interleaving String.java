5c09551aed62bf0e2ef0bd12a0f35af9f5c26de7
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-31 21:37:26 +0800

    Update 97. Interleaving String.java

--- 1-100/97. Interleaving String.java
+++ 1-100/97. Interleaving String.java
@@ -1,5 +1,13 @@
 // solution 1: backtracking, can only pass 99/101 of the test cases(too long input incurs time limit exceeded)
-// solution 2: dp
+
+// solution 2: dp, use 2D array to memorize some intermediate combinations' results so duplicate calculations are avoided
+// the key point here is to notify those duplicates. E.G, s1 = "aab", s2="aac", s3="aaabxx"
+// The recursive tree (i,j) would be (i,j stands for index of s1 and s2)
+// for a given combination 1,2, it will form a string with aaa which accordance with s3 currently, but for the next char you will
+// find in s3 is 'b', and the next char in both next index of s1 and s2 are not 'b'(this means 1,2 is invalid comb)
+// however, you may recalculate this comb 1,2 many times since u can reach this comb by from 0,1 --> 0,2 --> 1,2 or 0,1 --> 1,1 --> 1,2
+// or 1,0 --> 1,1 --> 1,2, thus incurs recalculations (remember the key point is avoidance of such recalculation, so a 2D array is 
+// necessary to record previous result)
 
 // solution 1
 class Solution {
@@ -20,3 +28,23 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length()!=s3.length()) return false;
+        int memo[][] = new int[s1.length()+1][s2.length()+1];
+        return recur(s1,s2,s3,0,0,memo);
+    }
+    public boolean recur(String s1,String s2,String s3,int i,int j,int memo[][]){
+        if(memo[i][j]>0) return memo[i][j]==1? true:false;
+        if(s1.length()==i) return s2.substring(j).equals(s3.substring(i+j));     
+        if(s2.length()==j) return s1.substring(i).equals(s3.substring(i+j)); 
+        
+        boolean ans = (s1.charAt(i)==s3.charAt(i+j)) && recur(s1,s2,s3,i+1,j,memo);
+        ans |= (s2.charAt(j)==s3.charAt(i+j)) && recur(s1,s2,s3,i,j+1,memo);
+        memo[i][j] = ans? 1:2;
+        return ans;
+    }
+}
+   
+    
