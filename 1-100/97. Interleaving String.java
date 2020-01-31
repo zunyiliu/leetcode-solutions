f0cf9ae30e1cae16fe7b474f7852175b50758d4f
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-01-31 19:45:28 +0800

    Create 97. Interleaving String.java

---
+++ 1-100/97. Interleaving String.java
@@ -0,0 +1,23 @@
+// solution 1: backtracking, can only pass 99/101 of the test cases(too long input incurs time limit exceeded)
+// solution 2: dp
+
+// solution 1
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length()!=s3.length()) return false;
+        if(s1.length()==0) return s2.equals(s3);
+        if(s2.length()==0) return s1.equals(s3);
+        if(s3.length()==0) return s1.length()==0 && s2.length()==0;
+        if(s1.charAt(0)==s2.charAt(0)){
+            if(s1.charAt(0)!=s3.charAt(0)) return false;
+            else return isInterleave(s1.substring(1),s2,s3.substring(1)) 
+                || isInterleave(s1,s2.substring(1),s3.substring(1));
+        }else{
+            if(s1.charAt(0)==s3.charAt(0)) 
+                return isInterleave(s1.substring(1),s2,s3.substring(1));
+            if(s2.charAt(0)==s3.charAt(0))
+                return isInterleave(s1,s2.substring(1),s3.substring(1));
+            return false;
+        }
+    }
+}
