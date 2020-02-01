bc6a27346acd473a0533d07aea874d432b4f844d
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-01 10:28:59 +0800

    Update 97. Interleaving String.java

--- 1-100/97. Interleaving String.java
+++ 1-100/97. Interleaving String.java
@@ -13,6 +13,8 @@
 // for any dp(i,j), its validity is determined by dp(i,j-1) and dp(i-1,j) with the corresponding recent char
 
 // solution 4: top-down dp
+
+// solution 5：same concept but use less memory size, only 1D array is still working
 // solution 1
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -91,4 +93,25 @@ class Solution {
         return dp[l1][l2];
     }
 }
-    
+
+// solution 5
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() != s1.length() + s2.length()) return false;
+        boolean dp[] = new boolean[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
+            }
+        }
+        return dp[s2.length()];
+    }
+}  
