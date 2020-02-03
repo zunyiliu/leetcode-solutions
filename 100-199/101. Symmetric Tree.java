13a1c83cb74e985ce8ccc2e4d1ab22927d20397b
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-03 18:30:58 +0800

    Update 101. Symmetric Tree.java

--- 100-199/101. Symmetric Tree.java
+++ 100-199/101. Symmetric Tree.java
@@ -2,7 +2,7 @@
 // 1st requirement is the root.val of the two trees are equal, then 1st tree's left subtree is symmetric with 2nd tree's right tree.
 // 1st tree's right tree is symmetric with 2nd tree's left tree. From now a recursion method is intuitive obtained.
 
-// solution 2:
+// solution 2: iterative solution using stack(queue also works)
 
 // solution 1
 class Solution {
@@ -15,5 +15,26 @@ class Solution {
         if( (left==null && right!=null) ||(left!=null&&right==null) ) return false;
         if(left.val!=right.val) return false;
         return sym(left.left,right.right) && sym(left.right,right.left);
+    }
+}
+
+// solution 2
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Stack<TreeNode> s = new Stack();
+        s.push(root);
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode t1 = s.pop();
+            TreeNode t2 = s.pop();
+            if(t1==null && t2==null) continue;
+            if( (t1!=null&&t2==null) || (t1==null&&t2!=null) ) return false;
+            if(t1.val != t2.val) return false;
+            s.push(t1.left);
+            s.push(t2.right);
+            s.push(t1.right);
+            s.push(t2.left);
+        }
+        return true;
     }
 }
