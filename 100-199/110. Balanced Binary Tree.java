c652beb70aaa85fe54c782d64cff03f66e740269
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-07 18:30:23 +0800

    Create 110. Balanced Binary Tree.java

---
+++ 100-199/110. Balanced Binary Tree.java
@@ -0,0 +1,22 @@
+// solution 1: high time complexity, recursively check if a node is balanced by measuring its left tree and right tree's height
+// if so then recursively check its left child and right child are also balanced
+
+// solution 1
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int h = Math.abs(height(root.left)-height(root.right));
+        if(h<2) return isBalanced(root.left) && isBalanced(root.right);
+        return false;
+    }
+    public int height(TreeNode node){
+        if(node==null) return 0;
+        else{
+            int l = 1+height(node.left);
+            int r = 1+height(node.right);
+            return Math.max(l,r);
+        }
+    }
+}
+
+// solution 2
