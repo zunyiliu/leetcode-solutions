839900a581ea23c5c99cdef6db586a26d7266619
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-07 13:28:18 +0800

    Update 109. Convert Sorted List to Binary Search Tree.java

--- 100-199/109. Convert Sorted List to Binary Search Tree.java
+++ 100-199/109. Convert Sorted List to Binary Search Tree.java
@@ -1,3 +1,7 @@
+// both solutions find the mid of the list first then recursively append its left and right subtree
+// different ways of finding mid of the list
+
+// solution 1
 class Solution {
     ListNode preMid = null;
     public TreeNode sortedListToBST(ListNode head) {
@@ -24,5 +28,24 @@ class Solution {
             head = head.next;
         }
         return head;
+    }
+}
+
+// solution 2
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return new TreeNode(head.val);
+        ListNode slow = head, pre = null, fast = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null; //cut left sub list here
+        TreeNode n = new TreeNode(slow.val);
+        n.left = sortedListToBST(head);
+        n.right = sortedListToBST(slow.next);
+        return n;
     }
 }
