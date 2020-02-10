5410f5da9345a185f0f99377fb3867552e8adbac
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-10 18:53:03 +0800

    Create 112. Path Sum.java

---
+++ 100-199/112. Path Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root==null) return false;
+        return recur(root,sum);
+    }
+    public boolean recur(TreeNode root,int sum){
+        if(root.left==null && root.right==null) return sum-root.val==0;
+        boolean l = false;
+        boolean r = false;
+        if(root.left!=null) l = recur(root.left,sum-root.val);
+        if(root.right!=null) r = recur(root.right,sum-root.val);
+        return l||r;
+    }
+}
