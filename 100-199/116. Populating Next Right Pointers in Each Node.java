2e7130a82a7e7002bd73d3b660c48e3bb2cf6547
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-12 15:03:53 +0800

    Update 116. Populating Next Right Pointers in Each Node.java

--- 100-199/116. Populating Next Right Pointers in Each Node.java
+++ 100-199/116. Populating Next Right Pointers in Each Node.java
@@ -1,3 +1,7 @@
+//solution 1: iterative solution, each time for a node root, iterate all nodes in current row and the children of the all nodes
+// start node from the left most node of the children then iterate again
+//solution 2: recursion
+//solution 3: another recursion
 class Solution {
     public Node connect(Node root) {
         Node p = root;
@@ -11,5 +15,23 @@ class Solution {
             root = root.left;
         }
         return p;
+    }
+}
+
+class Solution {
+    public Node connect(Node root) {
+        recur(root);
+        return root;
+    }
+    public void recur(Node root){
+        if(root!=null && root.left!=null){
+            Node cur = root;
+            while(cur!=null){
+                cur.left.next = cur.right;
+                cur.right.next = cur.next==null? null:cur.next.left;
+                cur = cur.next;
+            }
+            recur(root.left);
+        }
     }
 }
