a9e4f4afb3962f3f6837f68150b5c8bedd18f9df
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-20 10:51:43 +0800

    Update 124. Binary Tree Maximum Path Sum.java

--- 100-199/124. Binary Tree Maximum Path Sum.java
+++ 100-199/124. Binary Tree Maximum Path Sum.java
@@ -1,5 +1,8 @@
 // solution 1: naive dfs, the first kep point is that a path here can't fork, so, dfs all nodes, for each node assume it
 // is a starting root point and calculating the path sum, return the max one
+
+// solution 2: actually same conception, but the recursive method itself returns valuable result standing for the max sum path
+// a node can be extended (so duplicate visit of extended path is avoided)
 class Solution {
     int max = -9999999;
     public int maxPathSum(TreeNode root) {
@@ -34,5 +37,21 @@ class Solution {
             int res = Math.max(left,right)>0? Math.max(left,right):0;
             return res+n.val;
         }else return 0;
+    }
+}
+
+// solution 2
+class Solution {
+    int max = 0;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    public int dfs(TreeNode root){
+        if(root==null) return 0;
+        int left = Math.max(0,dfs(root.left));
+        int right = Math.max(0,dfs(root.right));
+        max = Math.max(max,left+right+root.val);
+        return Math.max(left,right)+root.val;
     }
 }
