5b7e7dd41094daa44a38df714e552d17bd177f89
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-02-25 20:47:59 +0800

    Create 141. Linked List Cycle.java

---
+++ 100-199/141. Linked List Cycle.java
@@ -0,0 +1,18 @@
+// O(1) space complexity, use two pointers fast and slow, fast moves 2 steps and slow moves 1 step each turn, if there's
+// a cycle there must be a point that fast catches up slow, which means fast==slow, if not there must be a point that fast 
+// reaches the end of the linked list
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null){
+            fast = fast.next;
+            if(fast==null) return false;
+            fast = fast.next;
+            slow = slow.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
