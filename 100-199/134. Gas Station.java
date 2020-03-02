0edafc48b9d4dbc68550de85fdcb43b2d2df50a9
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-02 12:57:47 +0800

    Update 134. Gas Station.java

--- 100-199/134. Gas Station.java
+++ 100-199/134. Gas Station.java
@@ -1,5 +1,5 @@
 // solution 1: naive solution, iterate all starting point and try all of them, O(n^2) time complexity
-// solution 2:
+// solution 2: brilliant solution O(n) time complexity
 
 // solution 1
 class Solution {
@@ -19,5 +19,25 @@ class Solution {
             if(total<0) return false;
         }
         return true;
+    }
+}
+
+// solution 2:
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int end = 0;
+        int st = gas.length-1;
+        int sum = gas[st]-cost[st];
+        
+        while(st>end){
+            if(sum>=0){
+                sum += gas[end]-cost[end];
+                end++;
+            }else{
+                st--;
+                sum += gas[st]-cost[st];                 
+            }
+        }
+        return sum >= 0? st:-1;
     }
 }
