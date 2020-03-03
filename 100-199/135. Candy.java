d0b9ed87a440825814f6266f7a4039f735f29e74
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-03 10:42:45 +0800

    Update 135. Candy.java

--- 100-199/135. Candy.java
+++ 100-199/135. Candy.java
@@ -1,6 +1,9 @@
 // solution 1: a straightforward solution, start from the smallest rate and go higher, O(nlogn) time comlexity for sorting
 // ratings, and O(n) for calculate --> overall O(nlogn) time complexity
 
+// solution 2: brute force O(n^2), 1st iteration, give all children 1 candy, 2nd iteration, give those children with higher
+// ratings than their number one more candy, so on and so force, maximum n traverse required
+
 class Solution {
     public int candy(int[] ratings) {
         // mapping for storing rate-index mapping
@@ -37,5 +40,32 @@ class Solution {
         // if current child's right has lower rating
         if(ratings[index+1]<ratings[index]) return candies[index+1]+1;
         else return 1;
+    }
+}
+
+// solution 2
+public class Solution {
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        boolean flag = true;
+        int sum = 0;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < ratings.length; i++) {
+                if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                    candies[i] = candies[i + 1] + 1;
+                    flag = true;
+                }
+                if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                    flag = true;
+                }
+            }
+        }
+        for (int candy : candies) {
+            sum += candy;
+        }
+        return sum;
     }
 }
