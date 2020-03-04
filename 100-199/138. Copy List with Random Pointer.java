710076d63fe3d2d37acd17daa08c9470086ee526
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-04 14:04:13 +0800

    Update 138. Copy List with Random Pointer.java

--- 100-199/138. Copy List with Random Pointer.java
+++ 100-199/138. Copy List with Random Pointer.java
@@ -1,6 +1,8 @@
 // similar as deep-copy of graph probelm, with the help of mapping to locate each node with its corresponding copy
 // solution 1: dfs
-// solution 2: bfs
+// solution 2: bfs with queue, this method applies for graph (since graph can not use .next to traverse all nodes, a queue
+// is necessary to traverse all nodes)
+// solution 3: bfs without applying queue, since node.next in linkedlist can traverse all nodes easily
 
 // solution 1
 class Solution {
@@ -22,3 +24,39 @@ class Solution {
     }
 }
 // solution 2
+class Solution {
+    public Node copyRandomList(Node head) {
+        HashMap<Node,Node> map = new HashMap();
+        return bfs(map,head);
+    }
+    public Node bfs(HashMap<Node,Node> map,Node node){
+        if(node==null) return null;
+        map.put(node,new Node(node.val));
+        
+        Queue<Node> queue = new LinkedList();
+        queue.offer(node);
+        while(!queue.isEmpty()){
+            Node cur = queue.poll();
+            if(cur.next==null) map.get(cur).next = null;
+            else{
+                if(!map.containsKey(cur.next)){ 
+                    map.put(cur.next,new Node(cur.next.val));
+                    queue.offer(cur.next);
+                }
+                map.get(cur).next = map.get(cur.next);
+            }
+            
+            if(cur.random==null) map.get(cur).random = null;
+            else{
+                if(!map.containsKey(cur.random)){ 
+                    map.put(cur.random,new Node(cur.random.val));
+                    queue.offer(cur.random);
+                }
+                map.get(cur).random = map.get(cur.random);
+            }
+        }
+        return map.get(node);
+    }
+}
+
+// solution 3
