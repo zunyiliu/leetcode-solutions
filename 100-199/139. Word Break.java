4e9505bdf26f9f01bc5d607e72ab1ac6ad3609e6
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-06 10:59:00 +0800

    Update 139. Word Break.java

--- 100-199/139. Word Break.java
+++ 100-199/139. Word Break.java
@@ -1,7 +1,9 @@
 // solution 1： DP, dp[i] stands for the s's subarray start from head with length i is a breakable word or not.
 // to verify dp[i] is true or false, iterate wordDict, and if(dp[i-str.length()] && str.equals(s.substring(i-str.length(),i)) )
 // then dp[i] = true
+// solution 2: similar
 
+// solution 1
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean dp[] = new boolean[s.length()+1];
@@ -13,6 +15,23 @@ class Solution {
                         dp[i] = true;
                         break;
                     }
+                }
+            }    
+        }
+        return dp[s.length()];
+    }
+}
+
+// solution 2
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean dp[] = new boolean[s.length()+1];
+        dp[0] = true;
+        for(int i=1;i<dp.length;i++){
+            for(int j=0;j<i;j++){
+                if(dp[j] && wordDict.contains(s.substring(j,i)) ){
+                    dp[i] = true;
+                    break;
                 }
             }    
         }
