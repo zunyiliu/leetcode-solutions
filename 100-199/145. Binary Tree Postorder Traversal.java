e3d1f3739ce34921c31bc05f05825c0e7da55a7f
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-09 15:29:29 +0800

    Update 145. Binary Tree Postorder Traversal.java

--- 100-199/145. Binary Tree Postorder Traversal.java
+++ 100-199/145. Binary Tree Postorder Traversal.java
@@ -1,7 +1,11 @@
 // in summary, post-order traverse is the hardest one while considering with iteration
 // solution 1: stack-based iterative solution 
 // solution 2: naive, recursion
+// solution 3: morris traverse(postorder), use a dummy head and let dummy.left = root, start from dummy head
+// do same morris traverse operation, the difference is while adding nodes, it starts from the 左转折点 then 
+// reverse traverse from the right-most node to itself.
 
+// solution 1
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList();
@@ -24,5 +28,57 @@ class Solution {
             reverselist.add(list.get(i));
         }
         return reverselist;
+    }
+}
+
+// solution 2
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList();
+        recur(root,list);
+        return list;
+    }
+    public void recur(TreeNode node,List<Integer> list){
+        if(node!=null){
+            recur(node.left,list);
+            recur(node.right,list);
+            list.add(node.val);
+        }
+    }
+}
+
+// solution 3
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        TreeNode dummy = new TreeNode(-1);
+        dummy.left = root;
+        root = dummy;
+        List<Integer> list = new ArrayList();
+        
+        while(root!=null){
+            TreeNode pre = root.left;
+            if(pre!=null){
+                while(pre.right!=null && pre.right!=root){
+                    pre = pre.right;
+                }
+                if(pre.right==null){
+                    pre.right = root;
+                    root = root.left;
+                }else{ // pre.right==root;
+                    pre.right = null;
+                    reverseAdd(root.left,list);
+                    root = root.right;
+                }
+            }else{
+                root = root.right;
+            }
+        }
+        return list;
+    }
+    public void reverseAdd(TreeNode node,List<Integer> list){
+        if(node!=null){
+            reverseAdd(node.right,list);
+            list.add(node.val);
+        }
     }
 }
