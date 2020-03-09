854e5ca7bd573dba902c12ba5706b65a84b5830b
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-09 12:36:50 +0800

    Update 144. Binary Tree Preorder Traversal.java

--- 100-199/144. Binary Tree Preorder Traversal.java
+++ 100-199/144. Binary Tree Preorder Traversal.java
@@ -1,5 +1,6 @@
 // solution 1: trivial -- recursion 
 // solution 2: naive -- stack based
+// solution 3: morris traverse, similar as morris inorder traverse
 
 // solution 1
 class Solution {
@@ -29,6 +30,35 @@ class Solution {
             list.add(cur.val);
             if(cur.right!=null) stack.push(cur.right);
             if(cur.left!=null) stack.push(cur.left);
+        }
+        return list;
+    }
+}
+
+// solution 3
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList();
+        
+        while(root!=null){
+            TreeNode pre = root.left;
+            if(pre!=null){
+                while(pre.right!=null && pre.right!=root){
+                    pre = pre.right;
+                }
+                // pre.right == null
+                if(pre.right==null){
+                    list.add(root.val);
+                    pre.right = root;
+                    root = root.left;
+                }else{ // pre.right == root
+                    pre.right = null;
+                    root = root.right;
+                }
+            }else{
+                list.add(root.val);
+                root = root.right;
+            }
         }
         return list;
     }
