54658161c4a9a5679ead2e2c60e48fda42082684
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-10 18:03:59 +0800

    Update 147. Insertion Sort List.java

--- 100-199/147. Insertion Sort List.java
+++ 100-199/147. Insertion Sort List.java
@@ -1,3 +1,7 @@
+// solution 1 and 2 are same logic, 2 from discussion forum, the better improvement of 2 is it's cleaner, organize a new list that stores
+// sorted nodes, while solution 1 operates sorting in-place
+
+// solution 1
 class Solution {
     public ListNode insertionSortList(ListNode head) {
         if(head==null) return null;
@@ -28,4 +32,33 @@ class Solution {
         }
         return cur;
     }
+}
+
+// solution 2
+class Solution {
+   public ListNode insertionSortList(ListNode head) {
+		if( head == null ){
+			return head;
+		}
+		
+		ListNode helper = new ListNode(0); //new starter of the sorted list
+		ListNode cur = head; //the node will be inserted
+		ListNode pre = helper; //insert node between pre and pre.next
+		ListNode next = null; //the next node will be inserted
+		//not the end of input list
+		while( cur != null ){
+			next = cur.next;
+			//find the right place to insert
+			while( pre.next != null && pre.next.val <= cur.val ){
+				pre = pre.next;
+			}
+			//insert between pre and pre.next
+			cur.next = pre.next;
+			pre.next = cur;
+			pre = helper;
+			cur = next;
+		}
+		
+		return helper.next;
+	}
 }
