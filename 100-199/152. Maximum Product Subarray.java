7265898bd35b4cb3ed812001e5db8615ffa0f7c3
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-13 16:26:12 +0800

    Update 152. Maximum Product Subarray.java

--- 100-199/152. Maximum Product Subarray.java
+++ 100-199/152. Maximum Product Subarray.java
@@ -1,7 +1,12 @@
-// dp: use two dp arrays to store max value and min value
+// solution 1: dp, use two dp arrays to store max value and min value
 // dpmax[i] stands for the max product of subarray starts from somewhere between index 0 to index i, and ends at index i;
 // dpmin[i] stands for the min product of subarray starts from somewhere between index 0 to index i, and ends at index;
 
+// solution 2: same dp concept as solution 1, the optimisation is the status in i is only influenced by the status in i-1
+// so, a max and a min that record current index is enough, from the cur min and max we can move forword to the next min and max
+
+
+// solution 1
 class Solution {
     public int maxProduct(int[] nums) {
         int dpmax[] = new int[nums.length];
@@ -24,5 +29,27 @@ class Solution {
             max = Math.max(max,dpmax[i]);
         }
         return max;
+    }
+}
+
+// solution 2
+class Solution {
+    public int maxProduct(int[] nums) {
+        int min = nums[0], max = nums[0], rt = nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            int a = max;
+            int b = min;
+            
+            max = Math.max(nums[i]*a,nums[i]*b);
+            max = Math.max(max,nums[i]);
+            
+            min = Math.min(nums[i]*a,nums[i]*b);
+            min = Math.min(min,nums[i]);
+
+            rt = Math.max(rt,max);
+        }
+        
+        return rt;
     }
 }
