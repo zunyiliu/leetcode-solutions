98debe3117b64837f2f140a63d2702e7a2d1a680
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-14 13:55:29 +0800

    Update 155. Min Stack.java

--- 100-199/155. Min Stack.java
+++ 100-199/155. Min Stack.java
@@ -49,3 +49,34 @@ class MinStack {
 }
 
 // solution 2
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> min;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack  = new Stack();
+        min = new Stack();
+        min.push(Integer.MAX_VALUE);
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if(x<=min.peek()) min.push(x);
+    }
+    
+    public void pop() {
+        if(min.peek().equals(stack.pop())) {
+            min.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+
+}
