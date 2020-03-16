4af7971c22e19fb2a1b7a8e5dfa41c1f4a4de038
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-16 17:08:48 +0800

    Create 166. Fraction to Recurring Decimal.java

---
+++ 100-199/166. Fraction to Recurring Decimal.java
@@ -0,0 +1,27 @@
+// use a map to record remainder/the remainder's appear index, and the index between 1st remainder and 2nd remainder is the part of 
+// the rational number's cycle(all fraction numbers are rational number)
+
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuilder result = new StringBuilder();
+        String sign = (numerator < 0 == denominator < 0 || numerator == 0) ? "" : "-";
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+        result.append(sign);
+        result.append(num / den);
+        long remainder = num % den;
+        if (remainder == 0)
+            return result.toString();
+        result.append(".");
+        HashMap<Long, Integer> hashMap = new HashMap<Long, Integer>();
+        while (!hashMap.containsKey(remainder)) {
+            hashMap.put(remainder, result.length());
+            result.append(10 * remainder / den);
+            remainder = 10 * remainder % den;
+        }
+        int index = hashMap.get(remainder);
+        result.insert(index, "(");
+        result.append(")");
+        return result.toString().replace("(0)", "");
+    }
+}
