2900831e79165fb082161114d18019abf9f4ecf1
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-03-24 18:08:28 +0800

    Update 164. Maximum Gap.java

--- 100-199/164. Maximum Gap.java
+++ 100-199/164. Maximum Gap.java
@@ -1,5 +1,11 @@
 // solution 1: not linear time complexity, use treeset to sort automatically, trivial
 
+// solution 2: linear time complexity, the basic idea is distribute numbers into bucket, with size of gap = (max-min)/nums.length-1
+// then you put numbers into buckets based on their value. It is assured that the largest gap among numbers must between different
+// bucket, so for each bucket the relatively large gap = next bucket's min value - this bucket's max, iterate buckets and get the
+// largest gap. (The worst case is all the nums are distributed evenly and each bucket holds one value, in this case you've just
+// use a traditional bucket sort to sort the value)
+
 
 // solution 1
 class Solution {
