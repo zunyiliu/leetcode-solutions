7525170d001376876bfabf44547ddc553bbd4b34
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-02 17:22:06 +0800

    Update 198. House Robber.java

--- 100-199/198. House Robber.java
+++ 100-199/198. House Robber.java
@@ -1,5 +1,5 @@
 // solution 1: bot-up
-// solution 2:
+// solution 2: top-down
 
 // solution 1
 class Solution {
@@ -16,5 +16,24 @@ class Solution {
             dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]); 
         }
         return dp[dp.length-1];
+    }
+}
+
+// solution 2
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length<=1){
+            return nums.length==1? nums[0]:0;
+        }
+        
+        Integer mem[] = new Integer[nums.length];
+        mem[0] = nums[0];
+        mem[1] = nums[0]>nums[1]? nums[0]:nums[1];
+        return dp(nums.length-1,nums,mem);
+    }
+    
+    public int dp(int index,int[] nums,Integer[] mem){
+        if(mem[index]!=null) return mem[index];
+        return mem[index] = Math.max(dp(index-1,nums,mem),dp(index-2,nums,mem)+nums[index]);
     }
 }
