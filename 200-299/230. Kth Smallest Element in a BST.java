ae5f6bfcf7e8021fcae7abf41093cdf5a257c14f
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-07 19:15:46 +0800

    Update 230. Kth Smallest Element in a BST.java

--- 200-299/230. Kth Smallest Element in a BST.java
+++ 200-299/230. Kth Smallest Element in a BST.java
@@ -1,4 +1,7 @@
-// inorder tree traversal
+// solution 1: inorder tree traversal, recursion
+// solution 2: morris traverse
+
+// solution 1
 class Solution {
     int count = 0;
     int res = -999;
@@ -16,5 +19,41 @@ class Solution {
             }
             inorder(root.right,k);
         }
+    }
+}
+
+// solution 2
+class Solution {
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        
+        while(root!=null){
+            if(root.left==null){
+                count++;
+                if(count==k){
+                    return root.val;
+                }else{
+                    root = root.right;
+                }
+            }else{
+                TreeNode cur = root.left;
+                while(cur.right!=null && cur.right!=root){
+                    cur = cur.right;
+                }
+                if(cur.right==root){
+                    cur.right = null;
+                    count++;
+                    if(count==k){
+                        return root.val;
+                    }else{
+                        root = root.right;
+                    }
+                }else{
+                    cur.right = root;
+                    root = root.left;
+                }
+            }
+        }
+        return -1;
     }
 }
