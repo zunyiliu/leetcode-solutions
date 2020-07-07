aed902d2fefa156e8569e19c97fb991cbf539dcd
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-07 13:55:45 +0800

    Update 212. Word Search II.java

--- 200-299/212. Word Search II.java
+++ 200-299/212. Word Search II.java
@@ -1,5 +1,7 @@
 // Solution 1: purely backtracking, can pass all test cases but take too long time
-// Solution 2:
+// Solution 2: write a implementation data structure of trie(prefix tree), then do the backtracking with trie
+// (the trie is implemented relatively complete, can apply the concept and modify the data structure to fullfill the requirement of the problem --> so that you
+// will have a less code length and better run time performance)
 
 // solution 1: try all possible scenarios with backtracking
 class Solution {
@@ -39,5 +41,106 @@ class Solution {
             visited[row][col] = false;
             return false;
         }
+    }
+}
+
+
+// solution 2
+class node{
+    char val;
+    boolean isWord;
+    node[] children;
+    public node(char val){
+        this.val = val;
+        this.children = new node[26];
+        this.isWord = false;
+    }
+}
+
+class trie{
+    node root;
+    public trie(){
+        this.root = new node('0');
+    }
+    
+    public void insert(String word){
+        node cur = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(cur.children[ch-'a']==null){
+                cur.children[ch-'a'] = new node(ch);
+            }
+            cur = cur.children[ch-'a'];
+        }
+        cur.isWord = true;
+    }
+    
+    public boolean startsWith(String word){
+        node cur = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(cur.children[ch-'a']==null){
+                return false;
+            }
+            cur = cur.children[ch-'a'];
+        }
+        return true;
+    }
+    
+    public boolean search(String word){
+        node cur = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(cur.children[ch-'a']==null){
+                return false;
+            }
+            cur = cur.children[ch-'a'];
+        }
+        return cur.isWord;
+    }
+    
+    public void remove(String word){
+        node cur = root;
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            cur = cur.children[ch-'a'];
+        }
+        cur.isWord = false;
+    }
+}
+
+class Solution {
+    trie tree = new trie();
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList();
+        for(int i=0;i<words.length;i++) tree.insert(words[i]);
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                backtrack(res,board,new String(),i,j);
+            }
+        }
+        
+        return res;
+    }
+    
+    public void backtrack(List<String> res,char[][] board,String str,int i,int j){
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length) return;
+        if(board[i][j]=='#') return;
+        
+        char c = board[i][j];
+        str += c;
+        if(!tree.startsWith(str)) return;
+        
+        if(tree.search(str)){
+            res.add(str);
+            tree.remove(str);
+        }
+        board[i][j] = '#';
+        backtrack(res,board,str,i+1,j);
+        backtrack(res,board,str,i-1,j);
+        backtrack(res,board,str,i,j+1);
+        backtrack(res,board,str,i,j-1);
+        board[i][j] = c;
     }
 }
