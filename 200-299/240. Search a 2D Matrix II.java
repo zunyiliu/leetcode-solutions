4a2d709f6674dc891b094f1ea1c2fd9e3d513e24
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-10 12:13:12 +0800

    Update 240. Search a 2D Matrix II.java

--- 200-299/240. Search a 2D Matrix II.java
+++ 200-299/240. Search a 2D Matrix II.java
@@ -1,6 +1,18 @@
 // solution 1: start from left-bot corner or right-top corner, move one step each time, O(n+m) complexity
 
-// solution 2: Binary search
+// solution 2: Divide and Conquer, divide the square into four parts, discard zone 1 if target > matrix[rowMid][colMid]
+// and discard zone 4 if target < matrix[rowMid][colMid]
+  zone 1      zone 2
+*  *  *  * | *  *  *  *
+*  *  *  * | *  *  *  *
+*  *  *  * | *  *  *  *
+*  *  *  * | *  *  *  *
+-----------------------
+*  *  *  * | *  *  *  *
+*  *  *  * | *  *  *  *
+*  *  *  * | *  *  *  *
+*  *  *  * | *  *  *  *
+  zone 3      zone 4
 
 
 // solution 1
@@ -21,3 +33,29 @@ class Solution {
 }
 
 // solution 2
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+
+        int right[] = new int[n];
+        int left[] = new int[n];
+        int res[] = new int[n-k+1];
+        
+        
+        for(int i=0;i<n;i++){
+            if(i%k==0) left[i] = nums[i];
+            else left[i] = Math.max(left[i-1],nums[i]);
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            if(i==n-1 || (i+1)%k==0 ) right[i] = nums[i];
+            else right[i] = Math.max(right[i+1],nums[i]);
+        }
+        
+        for(int i=0;i<res.length;i++){
+            res[i] = Math.max(right[i],left[i+k-1]);            
+        }
+        
+        return res;
+    }
+}
