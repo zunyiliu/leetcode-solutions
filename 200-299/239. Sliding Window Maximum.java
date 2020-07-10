6cdcb0c43d2338b439562df223f52480ed970c4f
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-10 10:37:34 +0800

    Update 239. Sliding Window Maximum.java

--- 200-299/239. Sliding Window Maximum.java
+++ 200-299/239. Sliding Window Maximum.java
@@ -2,10 +2,13 @@
 // solution 1: Apply PriorityQueue, time complexity O(n*K) (since remove takes O(k) in built-in Java priorityquque.remove())
 // if we implement remove ourselves, it will take O(n*logK)
 
-// solution 2: use the mind of deque, since remove in LinkedList also takes O(1), so use linkedlist also works
-// The concept is -- The LinkedList is storing elements from nums[] with their index sequence, 
-// basically for the nums[i] we are about to insert, first we move the leftmost element from linkedlist if it's out of window bound, then,
-// we continuously delete those rightmost elements in that are smaller than nums[i] 
+// solution 2: use the mind of deque, since remove in LinkedList also takes O(1), so use linkedlist also complete the alg in O(n) time
+// The concept is -- The LinkedList is storing elements from nums[] with their indices, 
+// basically, for the nums[i] we are about to insert, first we remove the leftmost element from linkedlist if it's out of window bound, then,
+// we continuously delete those rightmost elements that are smaller than nums[i](since these elements won't influence the sliding window maximum)
+// after that insert nums[i] into linkedlist. Last but not least, update res[i-k+1]'s value
+
+// solution 2.1: same as 2, use Deque instead of linkedlist
 
 // solution 3: Deque 
 // solution 3: DP with two arrays
@@ -45,6 +48,26 @@ class Solution {
             }
             list.add(i);
             if(i-k+1>=0) res[i-k+1] = nums[list.get(0)];
+        }
+        
+        return res;
+    }
+}
+
+// solution 2.1
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> d= new ArrayDeque();
+        int res[] = new int[nums.length-k+1];
+        
+        for(int i=0;i<nums.length;i++){
+            if(!d.isEmpty() && d.peekFirst()<=i-k) d.pollFirst();
+            
+            while(!d.isEmpty()&& nums[i]>nums[d.peekLast()] ){
+                d.pollLast();
+            }
+            d.offerLast(i);
+            if(i-k+1>=0) res[i-k+1] = nums[d.peekFirst()];
         }
         
         return res;
