c23c333ef0b2f704ba6fda5b3eef13da0ba10021
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-16 19:59:52 +0800

    Update 301. Remove Invalid Parentheses.java

--- 300-399/301. Remove Invalid Parentheses.java
+++ 300-399/301. Remove Invalid Parentheses.java
@@ -6,6 +6,7 @@
 //        you find the deleted number of left/right brackets are exceeding the limit, you can end this path-call in advance
 
 // solution 2: DFS(or backtracking)
+// solution 2.1: same as solution 2, use stringbuilder and in-place check of isValid(brackets) to same a lot run time
 
 // solution 1
 class Solution {
@@ -124,5 +125,55 @@ class Solution {
             } 
         }
         return l==0;
+    }
+}
+
+// solution 2.1
+class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        int rmL = 0, rmR = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                rmL++;
+            } else if (s.charAt(i) == ')') {
+                if (rmL != 0) {
+                    rmL--;
+                } else {
+                    rmR++;
+                }
+            }
+        }
+        Set<String> res = new HashSet<>();
+        dfs(s, 0, res, new StringBuilder(), rmL, rmR, 0);
+        return new ArrayList<String>(res);
+    }
+
+    public void dfs(String s, int i, Set<String> res, StringBuilder sb, int rmL, int rmR, int open) {
+        if (rmL < 0 || rmR < 0 || open < 0) {
+            return;
+        }
+        if (i == s.length()) {
+            if (rmL == 0 && rmR == 0 && open == 0) {
+                res.add(sb.toString());
+            }        
+            return;
+        }
+
+        char c = s.charAt(i); 
+        int len = sb.length();
+
+        if (c == '(') {
+            dfs(s, i + 1, res, sb, rmL - 1, rmR, open);		    // not use (
+            dfs(s, i + 1, res, sb.append(c), rmL, rmR, open + 1);       // use (
+
+        } else if (c == ')') {
+            dfs(s, i + 1, res, sb, rmL, rmR - 1, open);	            // not use  )
+            dfs(s, i + 1, res, sb.append(c), rmL, rmR, open - 1);  	    // use )
+
+        } else {
+            dfs(s, i + 1, res, sb.append(c), rmL, rmR, open);	
+        }
+
+        sb.setLength(len);        
     }
 }
