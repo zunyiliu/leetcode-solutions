0e1e3de04e8843c41c368b4cb7387bbfc61106b0
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-17 12:17:47 +0800

    Update 322. Coin Change.java

--- 300-399/322. Coin Change.java
+++ 300-399/322. Coin Change.java
@@ -3,6 +3,8 @@
 
 // solution 2: BFS
 
+// solution 3: DFS, since we are finding the path with shortest steps, BFS is much better as DFS will go through all paths thus exceeding the time limit
+
 // solution 1
 class Solution {
     public int coinChange(int[] coins, int amount) {
@@ -19,5 +21,26 @@ class Solution {
         }
         
         return dp[amount]==Integer.MAX_VALUE? -1:dp[amount];
+    }
+}
+
+
+// solution 3
+class Solution {
+    int res = Integer.MAX_VALUE;
+    public int coinChange(int[] coins, int amount) {
+        dfs(coins,amount,0);
+        return res==Integer.MAX_VALUE? -1:res;
+    }
+    
+    public void dfs(int[] coins,int amount,int count){
+        if(amount<0) return;
+        if(amount==0){
+            res = Math.min(count,res);
+            return;
+        }
+        for(int i=0;i<coins.length;i++){
+            dfs(coins,amount-coins[i],count+1);
+        }
     }
 }
