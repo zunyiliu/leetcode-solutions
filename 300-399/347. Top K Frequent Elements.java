142820cdc4fbbc41b6d0cb60df86ade9d1f707db
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-20 12:57:26 +0800

    Update 347. Top K Frequent Elements.java

--- 300-399/347. Top K Frequent Elements.java
+++ 300-399/347. Top K Frequent Elements.java
@@ -1,6 +1,8 @@
 // solution 1: hashmap + priorityQueue O(n), use hashmap to record all nums[i] with (key,value) --> (nums[i],nums[i]'s frequency)
 // use priorityQueue to pop the res according to keys' values from high to low K times, and that's the result
 
+// solution 2: hashmap + treemap O(n), treemap can use function pollLastEntry() to poll the last entry with the highest key
+
 // solution 1
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
@@ -37,3 +39,42 @@ class Solution {
         return res;
     }
 }
+
+// solution 2
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap();
+        
+        for(int n:nums){
+            if(map.containsKey(n)){
+                map.put(n, map.get(n)+1);
+            }else{
+                map.put(n,1);
+            }
+        }
+        
+        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+        for(int num : map.keySet()){
+           int freq = map.get(num);
+           if(!freqMap.containsKey(freq)){
+               freqMap.put(freq, new LinkedList<>());
+           }
+           freqMap.get(freq).add(num);
+        }
+        
+        
+        List<Integer> res = new ArrayList<>();
+        while(res.size()<k){
+            Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+            res.addAll(entry.getValue());
+        }
+        
+        int []tmp = new int[k];
+        for(int i=0;i<k;i++){
+            tmp[i] = res.get(i);
+        }
+        
+        return tmp;
+    }
+}
+
