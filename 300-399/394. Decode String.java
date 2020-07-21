f858fe7bbe3f1d427417e27bac150fe1365dadf9
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-21 13:02:01 +0800

    Create 394. Decode String.java

---
+++ 300-399/394. Decode String.java
@@ -0,0 +1,47 @@
+// A straight forward method wont record -- recursively iterate the string, in each recursion find a pair of brackets and decoding them,
+// return once no bracket pair can be found
+
+// solution 1: use two stacks, one for multiplier the other for strs in the corresponding multiplier, 
+// e.g for input "3[a2[c]]", stack A will store [3,2] and stack B will store [a,c]. After 1st decoding, they become [3] and [a], and the decoding res is "cc"
+// then we append the res of the 1st decoding to its previous inner-content, so the two stacks become to [3] and [acc], then they become [accaccacc] after 2nd iteration
+
+// solution 2: recursively solve the problem
+
+
+// solution 1
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> multis = new Stack();
+        Stack<StringBuilder> strs = new Stack(); 
+        int multi = 0;
+        // this is for storing the final res
+        strs.push(new StringBuilder());
+        
+        for(int i=0;i<s.length();i++){
+            
+            char ch = s.charAt(i);
+            
+            if(ch<='9' && ch>='0'){
+                multi = multi*10 + (ch-'0');
+            }else if(ch=='['){
+                multis.push(multi);
+                multi = 0;
+                strs.push(new StringBuilder());
+            }else if(ch==']'){
+                StringBuilder tmp = new StringBuilder();
+                StringBuilder content = strs.pop();
+                Integer iterate = multis.pop();
+                
+                for(int j=0;j<iterate;j++){
+                    tmp.append(content);
+                }
+                strs.push(strs.pop().append(tmp));
+            }else{
+                strs.push(strs.pop().append(ch));
+            }
+            
+        }
+        
+        return strs.pop().toString();
+    }
+}
