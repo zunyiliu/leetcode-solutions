35bd97bee110c13b7fea3c1e2067524f216e3654
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-21 13:57:20 +0800

    Update 394. Decode String.java

--- 300-399/394. Decode String.java
+++ 300-399/394. Decode String.java
@@ -5,8 +5,9 @@
 // e.g for input "3[a2[c]]", stack A will store [3,2] and stack B will store [a,c]. After 1st decoding, they become [3] and [a], and the decoding res is "cc"
 // then we append the res of the 1st decoding to its previous inner-content, so the two stacks become to [3] and [acc], then they become [accaccacc] after 2nd iteration
 
-// solution 2: recursively solve the problem
-
+// solution 2: recursively solve the problem, initially I come up with idea by using a for loop to iterating the string, and each time when meet a '[', call recursive function,
+// however, it's hard to track the index of corresponding ']' (each time after decoding as the string will change, thus the index of corresponding ']' will change too)
+// so the inner string witin a '[' ']' pair is hard to decode (will have higher run time complexity). But use queue can resolve this easily.
 
 // solution 1
 class Solution {
@@ -43,5 +44,40 @@ class Solution {
         }
         
         return strs.pop().toString();
+    }
+}
+
+// solution 2
+class Solution {
+    public String decodeString(String s) {
+        Queue<Character> q = new LinkedList();
+        for(char c:s.toCharArray()) q.add(c);
+        q.add(']');
+        return recur(q);
+    }
+    
+    public String recur(Queue<Character> q){
+        StringBuilder sb = new StringBuilder();
+        int multi = 0;
+                                            
+        while(!q.isEmpty()){
+            char ch = q.poll();
+                
+            if(ch<='9' && ch>='0'){
+                multi = multi*10 + (ch-'0');
+            }else if(ch=='['){
+                String res = recur(q);
+                for(int i=0;i<multi;i++){
+                    sb.append(res);
+                }
+                multi = 0;
+            }else if(ch==']'){
+                break;
+            }else{
+                sb.append(ch);
+            }
+        }
+        
+        return sb.toString();
     }
 }
