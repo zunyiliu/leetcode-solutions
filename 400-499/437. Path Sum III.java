d51e8b1bb62f996564fc02ef4adb32d384eb5084
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-07-31 14:07:53 +0800

    Update 437. Path Sum III.java

--- 400-499/437. Path Sum III.java
+++ 400-499/437. Path Sum III.java
@@ -22,3 +22,29 @@ class Solution {
         dfs(node.right,cur,sum);
     }
 }
+
+class Solution {
+    int count = 0;
+    public int pathSum(TreeNode root, int sum) {
+        start(root,sum);
+        return count;
+    }
+    
+    public void start(TreeNode root,int sum){
+        if(root==null) return;
+        start(root.left,sum);
+        start(root.right,sum);
+        
+        dfs(root,0,sum);
+    } 
+    
+    public void dfs(TreeNode root,int cur,int sum){
+        if(root==null) return;
+        
+        cur += root.val;
+        if(cur==sum) count++;
+        
+        dfs(root.left,cur,sum);
+        dfs(root.right,cur,sum);
+    }
+}
