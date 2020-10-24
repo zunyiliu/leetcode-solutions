8196d30dab91f9641d83818b5fb00aac1702cb2f
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-10-24 17:12:19 +0800

    Update 204. Count Primes.java

--- 200-299/204. Count Primes.java
+++ 200-299/204. Count Primes.java
@@ -1,5 +1,6 @@
 // use memorization, an boolean array that records all numbers smaller than n, where array[x] == false means x is not a prime, vice versa
 // start from i = 2, we can know all numbers i*j(j is not a integer) are not prime
+// make sure i*j shoud smaller than n, and be careful for edge case while i^2 is outrangede of Integer.MAX_VALUE -> this will incur i*j = a negative value that is smaller than n
 
 class Solution {
     public int countPrimes(int n) {
