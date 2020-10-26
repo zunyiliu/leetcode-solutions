1bfe0c442d4fee542a58b810a39a378be79c4980
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-10-26 11:44:27 +0800

    Create 201. Bitwise AND of Numbers Range.java

---
+++ 200-299/201. Bitwise AND of Numbers Range.java
@@ -0,0 +1,34 @@
+// solution 1: constant time O(1) solution, bit manipulation
+// 1. the range of m and n are between 0 to 2^31-1, we will calculate each bit's 1 or 0, and concatenate all 31 bits into the final number
+// 2. we know there're num = n-m+1 numbers from m to n, observe the i-th bit
+// 3. for 0-th bit, if num > 1 then 0-th bit must be 0(since there must be at least one 0)
+//    for 1-th bit, if num > 2 then 1-th bit must be 0
+//    for i-th bit, if num > 2^i, then i-th bit must be 0
+// 4. if num < 2^i, we calculate the least number m's i-th bit and most number n's i-th bit, if both of them are not 0 then the i-th bit of result is 1, otherwise is 0
+// 5. we do this bit by bit, with a for loop from 0 to 31, then we can joint the final result
+
+// solution 1
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int range = n-m+1;
+        int result = 0;
+        int gap = 0;
+        
+        for(int i=0;i<31;i++){
+            if(gap==0) gap = 1;
+            else gap *= 2;
+            if(gap<range) result += 0<<i;
+            else{
+                if((m>>i)%2==0 || (n>>i)%2==0){
+                    result += 0<<i;
+                }else{
+                    result += 1<<i;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
+
+// solution 2
