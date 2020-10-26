4f165b859646d5e35174b454bbc6141acbec0f93
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-10-26 19:16:09 +1030

    Update 205. Isomorphic Strings.java

--- 200-299/205. Isomorphic Strings.java
+++ 200-299/205. Isomorphic Strings.java
@@ -1,6 +1,9 @@
 // solution 1:
 // * read the decription carefully, be sure that 1. a char can only map to one char 2. now two chars can map to a same char *
+// set can be avoided as HashMap has an inner function called map.containsValue(value)
 // solution 2: same logic, use array instead of map
+// solution 3: same but avoid using both set and map, the actual method is to use an array to calculate the occurence of a char which is mapped by others
+// if two chars map to a same char then the occurence will become 2, thus return false
 
 // solution 1
 class Solution {
@@ -41,6 +44,29 @@ class Solution {
                 if(set.contains(ch1)) return false;
                 map[ch0-0] = ch1;
                 set.add(ch1);
+            }else{
+                if(map[ch0-0]!=ch1) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// solution 3
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char []map = new char[255];
+        int []m2 = new int[255];
+        if(s.length()!=t.length()) return false;
+        
+        for(int i=0;i<s.length();i++){
+            char ch0 = s.charAt(i);
+            char ch1 = t.charAt(i);
+            
+            if(map[ch0-0]==0){
+                if(m2[ch1-0]>0) return false;
+                map[ch0-0] = ch1;
+                m2[ch1-0]++;
             }else{
                 if(map[ch0-0]!=ch1) return false;
             }
