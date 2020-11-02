220946780165a3060540996ca413c328ad7f59b6
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-11-02 14:58:01 +0800

    Update 214. Shortest Palindrome.java

--- 200-299/214. Shortest Palindrome.java
+++ 200-299/214. Shortest Palindrome.java
@@ -7,7 +7,15 @@
 
 // solution 1: manacher algorithm
 
-// solution 2: KMP utilization to solve this problem, will explain later
+// solution 2: KMP utilization to solve this problem
+// 1. the goal of the problem is to find the longest palindrome starts from index 0 for input string s
+// 2. assume s = s1 + s2, s1 is the longest palindrome we are looking for
+// 3. init a new string ss = s + "#" + s.reverse(), which is s1 + s2 + '#' + s2.reverse() + s1.reverse()
+// 4. since s1 is a palindrome, then s1 == s1.reverse(), the "#" is for preventing the s.reverse() cross the border when expanding (e.g. when s = "aaaaa")
+// 5. now we are looking for the KMP look-up table t[ss.length()-1] in ss
+// 6. reminder: a KMP look-up table t[i] means the longest same prefix and postfix ending at index i(the prefix starts from head+1, and postfix ends at tail-1)
+
+// solution 2.1: same as 2, use recursion instead of while loop for creating KMP lool-up table
 
 // solution 1: manacher's algorithm, O(n) time copmplexity, please check explanation in problem 5.longest palindromic substring
 class Solution {
@@ -71,14 +79,38 @@ class Solution {
                 }
                 
                 if (ss.charAt(index) == ss.charAt(i)) {
-                    index++;
+                    table[i] = index + 1;
                 }
-                
-                table[i] = index;
             }
         }
         
         int longest = table[table.length-1];
         return new StringBuilder(s.substring(longest)).reverse().toString() + s;
+    }
+}
+
+// solution 2.1
+class Solution {
+    // KMP look up table -- find longest prefix and postfix
+    public String shortestPalindrome(String s) {
+        String ss = s + "#" + new StringBuilder(s).reverse().toString();
+        int t[] = new int[ss.length()];
+        
+        for (int i = 1; i < t.length; i++) {
+            if (ss.charAt(t[i-1]) == ss.charAt(i)) {
+                t[i] = t[i-1] + 1;
+            } else {
+                t[i] = recur(ss,t[i-1],ss.charAt(i),t);
+            }
+        }
+        
+        String addon = new StringBuilder(s.substring(t[t.length-1])).reverse().toString();
+        return addon + s;
+    }
+    
+    public int recur(String ss, int i, char ch, int[] t) {
+        if (i == 0) return ss.charAt(i) == ch? 1 : 0;
+        if(ss.charAt(i) == ch) return i + 1;
+        return recur(ss,t[i-1],ch,t);
     }
 }
