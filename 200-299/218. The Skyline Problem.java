10a189030c22024ca4e61fb58eb99159e724634c
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-11-03 18:00:24 +0800

    Update 218. The Skyline Problem.java

--- 200-299/218. The Skyline Problem.java
+++ 200-299/218. The Skyline Problem.java
@@ -1,3 +1,9 @@
+// solution 1 and 2 are same, but use different data structures, the solution 2 has a more concise logic
+// in solution 2, we use a -height representing a left line's height, and +height representing a right line's height
+// thus when sort the lines, we can let left lines are sorted in decending order and right lines are sorted in ascending order, with only one line of code
+
+// solution 1
+// there're many edge cases
 class Solution {
     class Line {
         int x;
@@ -72,6 +78,54 @@ class Solution {
                     tmp.add(max);
                     res.add(tmp);
                 }
+            }
+        }
+        
+        return res;
+    }
+}
+
+// solution 2
+class Solution {
+    class Line {
+        int x;
+        int y;
+        
+        public Line(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList();
+        List<Line> lines = new ArrayList();
+        
+        for (int i = 0; i < buildings.length; i++) {
+            lines.add(new Line(buildings[i][0],-buildings[i][2]));
+            lines.add(new Line(buildings[i][1],buildings[i][2]));
+        }
+        
+        Collections.sort(lines,(a,b) -> {
+            if (a.x != b.x) return a.x - b.x;
+            return a.y - b.y;
+        });
+        
+        Queue<Integer> queue = new PriorityQueue<Integer>((a,b) -> (b-a));
+        queue.add(0);
+        int max = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            int height = lines.get(i).y;
+            if (height < 0) queue.add(-height);
+            else queue.remove(height);
+            
+            int cur = queue.peek();
+            if (cur != max) {
+                max = cur;
+                List<Integer> tmp = new ArrayList();
+                tmp.add(lines.get(i).x);
+                tmp.add(queue.peek());
+                res.add(tmp);
             }
         }
         
