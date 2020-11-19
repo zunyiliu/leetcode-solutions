020f9f49fe4f34bc956a82573d15ca805e790d4e
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-11-19 10:57:26 +0800

    Create 223. Rectangle Area.java

---
+++ 200-299/223. Rectangle Area.java
@@ -0,0 +1,18 @@
+// 1. calculate area for rec1 and area for rec2
+// 2. calculate overlap area if it exists
+// 3. sum up the area of two recs and minus overlaop area
+
+class Solution {
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int a1 = (D - B) * (C - A);
+        int a2 = (H - F) * (G - E);
+        int overlap = 0;
+        
+        boolean lap1 = G > C && !(F > D || H < B || E > C);
+        boolean lap2 = G <= C && !(B > H || D < F || A > G);
+        if (lap1 || lap2) {
+            overlap = (Math.min(C,G) - Math.max(A,E)) * (Math.min(D,H) - Math.max(B,F));
+        }
+        return a1 + a2 - overlap;
+    }
+}
