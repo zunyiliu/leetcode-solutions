5812200dc3988e9d12c88a4b8633c6a469338411
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2020-11-30 10:56:41 +0800

    Update 225. Implement Stack using Queues.java

--- 200-299/225. Implement Stack using Queues.java
+++ 200-299/225. Implement Stack using Queues.java
@@ -1,3 +1,4 @@
+// two queues
 class MyStack {
     Queue<Integer> q1;
     Queue<Integer> q2;
@@ -18,6 +19,48 @@ class MyStack {
         Queue<Integer> tmp = q1;
         q1 = q2;
         q2 = tmp;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q1.poll();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q1.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+
+// one queue
+class MyStack {
+    Queue<Integer> q1;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1 = new LinkedList();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q1.offer(x);
+        for (int i = 0; i < q1.size()-1; i++) {
+            q1.offer(q1.poll());
+        }
     }
     
     /** Removes the element on top of the stack and returns that element. */
