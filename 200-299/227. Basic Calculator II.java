de6064d048df4489c1f65971aa076e35b2fd7f52
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2021-06-30 19:21:33 +0800

    Update 227. Basic Calculator II.java

--- 200-299/227. Basic Calculator II.java
+++ 200-299/227. Basic Calculator II.java
@@ -60,3 +60,39 @@ class Solution {
 }
 
 // solution 2:
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int num = 0;
+        char op = '+';
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if (ch <= '9' && ch >= '0') {
+                num = num * 10 + ch - '0';
+            } 
+            
+            if ( ((ch > '9' || ch < '0') && ch != ' ') || i == s.length()-1) {
+                if (op == '+') {
+                    stack.push(num);
+                } else if (op == '-') {
+                    stack.push(-num);
+                } else if (op == '*') {
+                    stack.push(stack.pop()*num);
+                } else if (op == '/') {
+                    stack.push(stack.pop()/num);
+                }
+                num = 0;
+                op = ch;
+            }
+        }
+        
+        int sum = 0;
+        for (int i : stack) {
+            sum += i;
+        }
+        
+        return sum;
+    }
+}
