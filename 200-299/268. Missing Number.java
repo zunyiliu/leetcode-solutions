9879d9a9f8465d9b13950eff70b63e9c3f45cfd2
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2021-07-19 11:52:21 +0800

    Create 268. Missing Number.java

---
+++ 200-299/268. Missing Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            res ^= nums[i];
+            res ^= i;
+        }
+        
+        return res ^= nums.length;
+    }
+}
