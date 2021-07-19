0331cba41049d4b4142a485e45bdff65f37d41fb
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2021-07-19 16:47:30 +0800

    Update 241. Different Ways to Add Parentheses.java

--- 200-299/241. Different Ways to Add Parentheses.java
+++ 200-299/241. Different Ways to Add Parentheses.java
@@ -1,3 +1,7 @@
+// solution 1: recursion
+// solution 2: recursion with DP, as there are many duplicated substrings, use a map to store those substrings as keys and their results as values
+
+// solution 1
 class Solution {
     public List<Integer> diffWaysToCompute(String exp) {
         List<Integer> list = new ArrayList();
@@ -21,6 +25,54 @@ class Solution {
             list.add(Integer.valueOf(exp));
         }
         
+        return list;
+    }
+}
+
+// solution 2
+class Solution {
+    Map<String,List<Integer>> map;
+    public List<Integer> diffWaysToCompute(String exp) {
+        map = new HashMap();
+        return recur(exp);
+    }
+    
+    public List<Integer> recur(String exp) {
+        List<Integer> list = new ArrayList();
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (ch == '-' || ch == '+' || ch == '*') {
+                String lexp = exp.substring(0,i);
+                String rexp = exp.substring(i+1);
+                List<Integer> left, right;
+                
+                if (map.containsKey(lexp)) {
+                    left = map.get(lexp);
+                } else {
+                    left = diffWaysToCompute(lexp);
+                }
+                
+                if (map.containsKey(rexp)) {
+                    right = map.get(rexp);
+                } else {
+                    right = diffWaysToCompute(rexp);
+                }
+                
+                for (Integer l: left) {
+                    for (Integer r: right) {
+                        if (ch == '-') list.add(l-r);
+                        else if (ch == '+') list.add(l+r);
+                        else list.add(l*r);
+                    }
+                }
+            }
+        }
+        
+        if (list.size()==0) {
+            list.add(Integer.valueOf(exp));
+        }
+        
+        map.put(exp,list);
         return list;
     }
 }
