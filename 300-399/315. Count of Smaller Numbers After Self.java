15e2cb275cd8b57c9e4762d6047373eaf3be39e6
Zunyi <43444919+Zunyi0719@users.noreply.github.com>
2021-08-10 15:39:53 +0800

    Update 315. Count of Smaller Numbers After Self.java

--- 300-399/315. Count of Smaller Numbers After Self.java
+++ 300-399/315. Count of Smaller Numbers After Self.java
@@ -1,5 +1,7 @@
 // solution 1: brute force
+// solution 2: merge sort, use merge sort, and when merge two sorted array during mergesort, calculate the number of smaller elements on each element's right
 
+// solution 1
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> res = new ArrayList();
@@ -16,5 +18,86 @@ class Solution {
         }
         
         return res;
+    }
+}
+
+// solution 2
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        int len = nums.length;
+        node[] nodes = new node[len];
+
+        // node.val is an element'value in nums
+        // node.index the index of the element in nums
+        // nodex.count is the count of smaller elements to the right of res[i][0]
+        for (int i = 0; i < len; i++) {
+            nodes[i] = new node(nums[i],i,0);
+        }
+
+        node[] ret = mergesort(nodes);
+        int [] res = new int[len];
+
+        for (int i = 0; i < ret.length; i++) {
+            res[ret[i].index] = ret[i].count;
+        }
+
+        List<Integer> listRes = new ArrayList();
+
+        for (int x : res) listRes.add(x);
+        return listRes;
+    }
+
+    public node[] mergesort(node[] nodes) {
+        int len = nodes.length;
+        if (len == 1) return nodes;
+
+        node[] left = copyNodes(nodes,0,(len-1)/2);
+        node[] right = copyNodes(nodes,(len-1)/2+1,len-1);
+        left = mergesort(left);
+        right = mergesort(right);
+        return merge(left,right);
+    }
+
+    public node[] merge(node[] left, node[] right) {
+        int l = 0;
+        int r = 0;
+        node[] ret = new node[left.length+right.length];
+
+        while (l < left.length || r < right.length) {
+            int leftVal = l == left.length? 999999 : left[l].val;
+            int rightVal = r == right.length? 999999 : right[r].val;
+
+            if (leftVal <= rightVal) {
+                ret[l+r] = left[l];
+                ret[l+r].count += r;
+                l++;
+            } else {
+                ret[l+r] = right[r];
+                r++;
+            }
+        }
+        return ret;
+    }
+
+    public node[] copyNodes(node[] nodes, int st, int end) {
+        node[] ret = new node[end-st+1];
+
+        for (int i = 0; i <= end-st; i++) {
+            ret[i] = nodes[i+st];
+        }
+
+        return ret;
+    }
+
+    class node {
+        int val;
+        int index;
+        int count;
+
+        public node(int val, int index, int count) {
+            this.val = val;
+            this.index = index;
+            this.count = count;
+        }
     }
 }
